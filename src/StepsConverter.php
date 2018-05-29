9e39fb44dfb7bdf77639763fd5b0b3f73b5907d9
Mohamed Ali <mmorsheedy@mobiledoctors24-7.com>
2018-05-29 12:43:42 +0200

    add steps as reading and add converter

---
+++ src/StepsConverter.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Hagag\MedicalConversion;
+
+class StepsConverter  extends MedicalConversion
+{
+    private $from;
+    private $to;
+    private $value;
+    public function __construct()
+    {
+    }
+
+    public function convert($from,$to,$value){
+       return $value;
+    }
+
+    /**
+     * @param mixed $from
+     */
+    public function setFrom($from)
+    {
+        $this->from = $from;
+    }
+
+    /**
+     * @param mixed $to
+     */
+    public function setTo($to)
+    {
+        $this->to = $to;
+    }
+
+    /**
+     * @param mixed $value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+
+
+}
